1bbc23ab7c5b512e68566cb8ed55f2ac5e57c7ad
David Halter <davidhalter88@gmail.com>
2013-01-18 18:41:05 +0100

    only rst files are allowed in archives, now

--- rstblog/modules/blog.py
+++ rstblog/modules/blog.py
@@ -72,7 +72,8 @@ def process_blog_entry(context):
             if rv is not None:
                 context.pub_date = datetime(*rv)
 
-    if context.pub_date is not None:
+    if context.pub_date is not None \
+            and context.source_filename.endswith('.rst'):
         context.builder.get_storage('blog') \
             .setdefault(context.pub_date.year, {}) \
             .setdefault(('0%d' % context.pub_date.month)[-2:], []) \
